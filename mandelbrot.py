0a60470b7e44fc38f6516ebe31b5ccf5a143dd4e
Pabisz <sfrolu23@student.aau.dk>
2026-02-22 18:03:36 +0100

    Added mandelbrot skeleton

---
+++ mandelbrot.py
@@ -0,0 +1,21 @@
+"""
+Mandelbrot Set Generator
+
+Author: [Sebastian Pabisz Frolund]
+Course: Numerical Scientific Computing 2026
+"""
+def: f(x):
+    """
+    Example function.
+
+    Parameters
+    ----------
+    x : float
+        Input value
+
+    Returns
+    -------
+    float
+        Output value
+    """
+    pass
