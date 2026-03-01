38c5dd2ca8b312df3c0c885c43bc272a33fbb4e8
Pabisz <sfrolu23@student.aau.dk>
2026-03-01 18:59:37 +0100

    Fil der laver function-level profiling

---
+++ mandelbrot_profile.py
@@ -0,0 +1,9 @@
+import cProfile , pstats
+from mandelbrot import mandelbrot_set_naive , mandelbrot_set_vectorized
+cProfile.run ("mandelbrot_set_naive ( -2 , 1, -1.5 , 1.5 , 512 , 512, 100)", "naive_profile.prof")
+cProfile.run ("mandelbrot_set_vectorized ( -2 , 1, -1.5 , 1.5 , 512 , 512, 100)", 'vectorized_profile.prof')
+
+for name in ('naive_profile.prof', 'vectorized_profile.prof'):
+    stats = pstats.Stats(name)
+    stats.sort_stats('cumulative')
+    stats.print_stats(10)
