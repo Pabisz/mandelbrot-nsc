9d9308327caa0cd7bbc262c2474b81b25bc7096d
Pabisz <sfrolu23@student.aau.dk>
2026-03-01 20:05:09 +0100

    Tilføjet fil for at profilere naiv implementation linje for linje

---
+++ mandelbrot_lineprofile.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+@profile
+def mandelbrot_set_naive_lineprofile(xmin, xmax, ymin, ymax, width, height, max_iter=100):
+    x = np.linspace(xmin, xmax, width)
+    y = np.linspace(ymin, ymax, height)
+    result = np.zeros((height, width), dtype=int)
+    for i in range(height):
+        for j in range(width):
+            c = x[j] + 1j * y[i]
+            z = 0
+            for n in range(max_iter):
+                if abs(z) > 2:
+                    result[i, j] = n
+                    break
+                z = z*z + c
+            else:
+                result[i, j] = max_iter
+    return result
+
+if __name__ == "__main__":
+    mandelbrot_set_naive_lineprofile(-2, 1, -1.5, 1.5, 512, 512, 100)
