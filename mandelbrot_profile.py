e49a8a36fec89f316caea686a8f216c7496d77b6
Pabisz <sfrolu23@student.aau.dk>
2026-04-27 22:45:34 +0200

    Tilføjet header

--- mandelbrot_profile.py
+++ mandelbrot_profile.py
@@ -7,8 +7,8 @@ Course: Numerical Scientific Computing 2026
 import cProfile , pstats
 from mandelbrot import mandelbrot_set_naive , mandelbrot_set_vectorized
 
-cProfile.run ("mandelbrot_set_naive ( -2 , 1, -1.5 , 1.5 , 512 , 512, 100)", "naive_profile.prof")
-cProfile.run ("mandelbrot_set_vectorized ( -2 , 1, -1.5 , 1.5 , 512 , 512, 100)", 'vectorized_profile.prof')
+cProfile.run ("mandelbrot_set_naive ( -2 , 1, -1.5 , 1.5 , 1024 , 1024, 100)", "naive_profile.prof")
+cProfile.run ("mandelbrot_set_vectorized ( -2 , 1, -1.5 , 1.5 , 1024 , 1024, 100)", 'vectorized_profile.prof')
 
 for name in ('naive_profile.prof', 'vectorized_profile.prof'):
     stats = pstats.Stats(name)
